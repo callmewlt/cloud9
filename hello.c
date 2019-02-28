9494b97301d510b8e6049c9790734a968bc2857c
root <callme_wlt@qq.com>
2019-02-28 03:48:35 +0000

    zaaaa

--- hello.c
+++ hello.c
@@ -5,6 +5,10 @@ int main(int argc,char **argv)
     char a[10]={"hello\n"};
     printf(a);
     printf(a);
+    
+    printf(a);
+    printf(a);
+    
     printf("Hello,Linux.\n");
     exit(0);
 }
